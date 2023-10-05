6e662849717192a5ce2c8cf90fa7e0cccdb9bf5d
Vito <lolozol80xd@gmail.com>
2023-10-05 09:01:00 +0200

    2nd commit

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,11 +1,11 @@
-// package controller;
+package controller;
 
-// import model.Filereader;
-// import view.MainConsole;
+import model.Filereader;
+import view.MainConsole;
 
-// public class MainController {
-//     public MainController(){
-//         new MainConsole().showData(new Filereader().loadData());
+public class MainController {
+    public MainController(){
+        new MainConsole().showData(new Filereader().loadData());
 
-//     }
-// }
+    }
+}
